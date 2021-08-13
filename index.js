0e4af521ce0672d517ff1a6a1b556808f5a6b1f9
raphaelbmesquita123 <raphaelbmesquita@gmail.com>
2021-08-13 18:49:49 +0100

    update

---
+++ index.js
@@ -0,0 +1,37 @@
+require('dotenv').config({path:__dirname+'/../.env'})
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const stripe = require("stripe")(process.env.NODE_STRIPE_SECRET);
+
+app.use(express.static("."));
+app.use(express.json());
+app.use(cors({ origin: process.env.URL_AUTORIZED}))
+
+const calculateOrderAmount = items => {
+    if(items.length === 1){
+        return items[0].price * 100
+    } else {
+        const fullPrice = items.reduce((acc, item) => acc.price + item.price)
+        return fullPrice * 100;
+    }
+};
+
+app.post("/create-payment-intent", async (req, res) => {
+    const { items } = req.body;
+    try{
+        const paymentIntent = await stripe.paymentIntents.create({
+            amount: calculateOrderAmount(items),
+            currency: "eur"
+        });
+
+        res.send({
+            clientSecret: paymentIntent.client_secret
+        });
+        
+    } catch(err){
+        console.log(err)
+    }
+});
+
+app.listen(process.env.PORT || 4242, () => console.log(' port 4242!'));
